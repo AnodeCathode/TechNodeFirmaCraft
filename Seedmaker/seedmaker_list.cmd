b355e2b91badd9109709c81a114041d4b15c1749
AnodeCathode <shotorius_dt@hotmail.com>
2020-08-15 07:32:41 -0600

    Add some more reference data

---
+++ Seedmaker/seedmaker_list.cmd
@@ -0,0 +1,2 @@
+title SeedMaker
+java net.dries007.tfc.seedmaker.MainLegend -cp TFCSeedMaker-0.1.0.jar 
---
+++ Seedmaker/seedmakerlegend.cmd
@@ -0,0 +1,2 @@
+title SeedMaker
+java -cp TFCSeedMaker-0.1.0.jar net.dries007.tfc.seedmaker.MainLegend 
