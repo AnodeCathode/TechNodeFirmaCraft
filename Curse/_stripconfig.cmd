7aede441a5a12817b43d15d7adfce07539801a2f
AnodeCathode <shotorius_dt@hotmail.com>
2021-04-09 11:56:10 -0600

    Curse Update

---
+++ Curse/_stripconfig.cmd
@@ -0,0 +1,5 @@
+del overrides\config\jei\worldSettings.cfg
+del overrides\config\jei\bookmarks.ini
+del overrides\config\splash.properties
+del overrides\config\worldedit\worldedit.properties
+pause
